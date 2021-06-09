06c5180440775697d8474bbaa9cd4f40063ed8e5
Drigo <rodrigoaguayo94@gmail.com>
2021-06-08 19:21:29 -0700

    added initial inquirer prompt questions for users

--- employeeTracker.js
+++ employeeTracker.js
@@ -18,3 +18,24 @@ const connection = mysql.createConnection({
 connection.connect((err) => {
   if (err) throw err;
 });
+
+const mainHub = () =>{
+    inquirer.prompt({
+        name: 'initial',
+        type: 'list',
+        message: 'Which of the following would you like to do?',
+        choices: [
+            'Add an additional department?',
+            'Add an additional role?',
+            "Add an additoinal employee?",
+            "View a department?",
+            'View one of the roles?',
+            'View one of the employees?'
+        ]
+    })
+    .then((answer) => {
+        switch(answer.initial){
+            
+        }
+    })
+}
